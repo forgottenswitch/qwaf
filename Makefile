25d10026b629d5375500f76a6c87e189172eee9a
Mihail Konev <k.mvc@ya.ru>
2016-11-05 21:26:03 +0500

    parse layouts

--- Makefile
+++ Makefile
@@ -1,8 +1,10 @@
-all: gen
+OUTDIR = gen
 
-gen: conv.py compat symbols types hjkl layouts letters_latin letters_cyr
-	 python3 conv.py -o gen layouts
+all: $(OUTDIR)
+
+$(OUTDIR): conv.py compat symbols types hjkl layouts letters_latin letters_cyr
+	 python3 conv.py -o $@ layouts
 
 .PHONY: clean
 clean:
-	@rm -r gen
+	@rm -r $(OUTDIR)
--- conv.py
+++ conv.py
@@ -42,14 +42,56 @@ indir = os.path.dirname(infile)
 layouts = []
 partials = []
 
+class Keydefs:
+    def __init__(self, filename, name):
+        self.filename = filename
+        self.name = name
+        self.includes = []
+        self.keys = []
+
+    def __str__(self):
+        return """Keydefs< '{}({})' {}\n {}>""".format(
+                self.filename, self.name,
+                self.includes, self.keys)
+
 def read_file(filename, as_partials=False):
+    filename_name = os.path.basename(filename)
+
+    def syntax_error(msg):
+        nonlocal filename, nl
+        fatal("{}:{}: {}".format(filename, nl, msg))
+
+    def check_for_cur_defs():
+        nonlocal filename, nl, cur_defs
+        if not cur_defs:
+            fatal("{}:{}: need to be in 'symbols'".format(filename, nl))
+
     with open(filename, "r") as f:
         lines = f.readlines()
+    cur_defs = None
     for nl, l in enumerate(lines, 1):
         toks = shlex.split(l)
         print(str(toks))
+        if len(toks):
+            tok, *args = toks
+            if tok == '#':
+                True
+            elif tok == "symbols" and not as_partials:
+                if len(args) < 1:
+                    syntax_error("expected argument to 'symbols'")
+                cur_defs = Keydefs(filename_name, args[0])
+                layouts.append(cur_defs)
+            elif tok == "include":
+                if len(args) < 1:
+                    syntax_error("expected argument to 'include'")
+                cur_defs.includes.append(args[0])
+            else:
+                syntax_error("unknown directive '{}'".format(tok))
 
 read_file("layouts")
+for l in layouts:
+    print(l)
+exit()
 
 if not os.path.exists(outdir):
     os.mkdir(outdir)
