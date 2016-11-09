7a9114cac617eaee41863705f52ae349ac9f0097
Mihail Konev <k.mvc@ya.ru>
2016-11-10 02:33:48 +0500

    xkb: output 'qwaf' file, rather than as-in-source

--- conv/xkb.py
+++ conv/xkb.py
@@ -1,22 +1,30 @@
 import os
+import re
 
 name = "xkb"
+
+def filter_include(inc):
+    inc = re.sub(r"^(letters|layouts)", "qwaf", inc)
+    return inc
 
 def convert(debug, outdir, keydefs, layouts, partials):
     for x in ["symbols", "types", "compat"]:
         os.mkdir(os.path.join(outdir, x))
-    for k in layouts:
-        includes = ""
-        for inc in k.includes:
-            includes += "  include '{}'\n".format(inc)
-        filename = os.path.join(outdir, "symbols", k.filename)
-        with open(filename, "a") as f:
-            xkb_symbols = """partial alphanumeric
+    filename = os.path.join(outdir, "symbols", "qwaf")
+    with open(filename, "w") as f:
+        for k in layouts:
+            includes = ""
+            for inc in k.includes:
+                inc = filter_include(inc)
+                includes += "  include '{}'\n".format(inc)
+            with open(filename, "a") as f:
+                xkb_symbols = """partial alphanumeric
 xkb_symbols '{}' {{
 {}}};
 
 """.format(k.name, includes)
-            if debug:
-                print("Into {}:".format(filename))
-                print(xkb_symbols, end="")
-            f.write(xkb_symbols)
+                if debug:
+                    print("Into {}:".format(filename))
+                    print(xkb_symbols, end="")
+                f.write(xkb_symbols)
+
