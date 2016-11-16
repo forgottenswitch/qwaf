61e10e58bbaef29b648e627cc4e7252fad19c877
Mihail Konev <k.mvc@ya.ru>
2016-11-16 22:01:10 +0500

    svg: add dividing line

--- conv/svg.py
+++ conv/svg.py
@@ -17,12 +17,34 @@ def convert(debug, outdir, keydefs, layouts, partials):
     key_h = key_w
     key_b = 20
 
+    tilde_mul = 1.0
+    tab_mul = 1.5
+    caps_mul = 1.8
+    lshift_mul = 2.4
+
     for lt in layouts:
         keys = []
+
+        # r/l side dividing line
+        muls = [tilde_mul, tab_mul, caps_mul, lshift_mul]
+        for row, mul in enumerate(muls):
+            xpos1 = key_b + key_w * mul + (key_b + key_w) * 5 + key_b * 0.5
+            try:
+                xpos2 = xpos1 + key_w * (muls[row+1] - mul)
+            except:
+                xpos2 = None
+            ypos1 = (key_b + key_h) * row + key_b*0.5
+            ypos2 = ypos1 + key_h + key_b
+            keys.append("""<line x1="{}" y1="{}" x2="{}" y2="{}" stroke="black" stroke-width="{}" />""".format(xpos1, ypos1, xpos1, ypos2, key_b*0.1 ))
+            if xpos2:
+                keys.append("""<line x1="{}" y1="{}" x2="{}" y2="{}" stroke="black" stroke-width="{}" />""".format(xpos1, ypos2, xpos2, ypos2, key_b*0.1 ))
+
         for k in lt.compiled_keys:
             directive, *args = k
+
             if directive == "key":
                 keycode, keysyms = args
+
                 if keycode[:2] in ["AB", "AC", "AD", "AE"] or keycode in ["TLDE", "BKSL"]:
                     if keycode == "TLDE":
                         column = 0
@@ -31,16 +53,16 @@ def convert(debug, outdir, keydefs, layouts, partials):
                     else:
                         column = int(keycode[2:])
                     if keycode.startswith("AB"):
-                        xpos = int(key_w * 2.4 + (key_b + key_w) * (column-1) + key_b * 2)
+                        xpos = int(key_w * lshift_mul + (key_b + key_w) * (column-1) + key_b * 2)
                         ypos = int((key_b + key_w) * 3 + key_b)
                     elif keycode.startswith("AC"):
-                        xpos = int(key_w * 1.8 + (key_b + key_w) * (column-1) + key_b * 2)
+                        xpos = int(key_w * caps_mul + (key_b + key_w) * (column-1) + key_b * 2)
                         ypos = int((key_b + key_w) * 2 + key_b)
                     elif keycode.startswith("AD"):
-                        xpos = int(key_w * 1.5 + (key_b + key_w) * (column-1) + key_b * 2)
+                        xpos = int(key_w * tab_mul + (key_b + key_w) * (column-1) + key_b * 2)
                         ypos = int((key_b + key_w) * 1 + key_b)
                     else:
-                        xpos = int(key_w * 1.0 + (key_b + key_w) * (column-1) + key_b * 2)
+                        xpos = int(key_w * tilde_mul + (key_b + key_w) * (column-1) + key_b * 2)
                         ypos = int(key_b)
 
                     ksym = str(keysyms[0]).capitalize()
