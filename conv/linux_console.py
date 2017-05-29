forgottenswitch/qwaf

commit f99fd827176d1e1eccc68cdfbf03b5605339bf3c

hjkl: remove redirs workaround
While it was functional, it could have still, rarely, stop the keys from
working under Xserver.

Fix the Uffff matching code not to accept "Up".

conv/linux_console.py
@@ -85,26 +85,6 @@ Digits = {
 
 Unicode_re = re.compile(r"U[0-9a-fA-F]{1,4}$")
 
-Redirs = {
-        # This should be kept in sync with "redirs" from ./hjkl
-        "ISO_Fast_Cursor_Left": "Left",
-        "ISO_Fast_Cursor_Down": "Down",
-        "ISO_Fast_Cursor_Up": "Up",
-        "ISO_Fast_Cursor_Right": "Right",
-        #
-        "ISO_Partial_Line_Down": "Next",
-        "ISO_Partial_Line_Up": "Prior",
-        "ISO_Partial_Space_Left": "Home",
-        "ISO_Partial_Space_Right": "End",
-        #
-        "ISO_Set_Margin_Left": "BackSpace",
-        "ISO_Set_Margin_Right": "Delete",
-        #
-        "ISO_Release_Margin_Left": "Return",
-        "ISO_Release_Margin_Right": "Insert",
-        "ISO_Release_Both_Margins": "Escape",
-}
-
 UnknownSymnames = [
         "horizconnector", "schwa", "SCHWA",
         "leftdoublequotemark", "rightdoublequotemark",
@@ -259,8 +239,7 @@ def one_layout(kl, kl_qwerty, dest_fobj, dual, keycodes_defined):
                 if not modifier: key_def_lines.append("#")
                 key_def_lines.append("# {} <{}>".format(mod_capit, keycode));
                 if not modifier:
-                    keysyms1 = [ Redirs.get(x) or x for x in keysyms ]
-                    key_def_lines.append("# " + str(keysyms1))
+                    key_def_lines.append("# " + str(keysyms))
                 if not modifier: key_def_lines.append("#")
 
                 linux_kcode = 0
@@ -295,8 +274,6 @@ def one_layout(kl, kl_qwerty, dest_fobj, dual, keycodes_defined):
                         ksym = Digits[ksym]
                     elif dual and ksym in GroupTranslations:
                         ksym = GroupTranslations[ksym]
-                    elif ksym in Redirs:
-                        ksym = Redirs[ksym]
                     elif ksym and re.match(Unicode_re, ksym):
                         ksym_code_hex = ksym[1:]
                         ksym_code = int(ksym_code_hex, base=16)
@@ -319,9 +296,7 @@ def one_layout(kl, kl_qwerty, dest_fobj, dual, keycodes_defined):
                             print(SymnameDefs)
                             raise e
                         ksym = "U+" + int_to_base(ksym_utf, 16).zfill(4)
-
-                    # not elif because of Redirs
-                    if ksym in ["Next", "Prior"] and (lv % 2) == 0:
+                    elif ksym in ["Next", "Prior"] and (lv % 2) == 0:
                         if ksym == "Next":  ksym = "Decr_Console"
                         if ksym == "Prior": ksym = "Incr_Console"
 

conv/svg.py
@@ -58,6 +58,8 @@ def convert(debug, outdir, symname_defs, layouts, partials):
                             svg_ops="\n".join(svg_ops)
                             ))
 
+U_hexdigit = re.compile("U[0-9a-fA-F]+$")
+
 def output_key(svg_ops, keycode, keysym1, keysym2, symname_defs, lt, debug):
     if debug:
         print(["svg: lkey", lt.name, keycode, keysym1, keysym2])
@@ -93,7 +95,7 @@ def output_key(svg_ops, keycode, keysym1, keysym2, symname_defs, lt, debug):
             if s in special_ksyms:
                 return special_ksyms[s]
             if s not in symname_defs:
-                if s.startswith("U"):
+                if re.match(U_hexdigit, s):
                     code = int(s[1:], base=16)
             else:
                 code = symname_defs.get(s)

examples/run_xkb_example.sh
@@ -67,25 +67,9 @@ if test -z "$nth_src" ; then
   exit 1
 fi
 
-xserver_handles_redirs=y
-if X -version 2>&1 | grep -q "1.18.[1234]" ; then
-  xserver_handles_redirs=n
-fi
-
 temp_xkb=$(mktemp)
 cp "${thisdir}/qwaf_hjkl.xkb" "$temp_xkb"
 sed -i -e "s/hjkl(qwaf)+ctrl(nocaps)/$nth_src/" "$temp_xkb"
-
-# For Xorg 1.18.1 - 1.18.4, add "+hjkl(lv5)" to xkb_symbols
-if test _"$xserver_handles_redirs" != _y ; then
-  hjkl_tail="+hjkl(lv5)"
-  for lv in 2 3 4; do
-    if grep -q "xkb_symbols.*:$lv" "$temp_xkb" ; then
-      hjkl_tail="${hjkl_tail}+hjkl(lv5):$lv"
-    fi
-  done
-  sed -i -e 's/\(.*xkb_symbols.*\)\(\"[^"]*$\)/\1+hjkl(redirs)'"$hjkl_tail"'\2/' "$temp_xkb"
-fi
 
 echo_and_run() {
   echo "$@"

hjkl
@@ -2,14 +2,12 @@
 
 symbols "shift"
   include "hjkl(lv_1_3_5)"
-  include "hjkl(redirs)"
   include "level3_hjkl(comma_latch)"
   include "level5_hjkl(semic_shift)"
   include "level5_hjkl(ralt_lock)"
 
 symbols "lock"
   include "hjkl(lv_1_3_5)"
-  include "hjkl(redirs)"
   include "level3_hjkl(comma_latch)"
   include "level5_hjkl(semic_lock)"
   include "level5_hjkl(ralt_lock)"
@@ -148,28 +146,3 @@ symbols "latching_shifts"
 
   key RTSH: ISO_Level2_Latch, ISO_Level2_Latch
   key LFSH: ISO_Level2_Latch, ISO_Level2_Latch
-
-# "lv5" implemented with RedirectKey.
-# Requires "hjkl" in the xkb_compat.
-# Fixes Shift+Cursor Keys selection not working in MonoDevelop.
-# Does not work with Xserver 1.18.1 - 1.18.4
-symbols "redirs"
-  level 5
-
-  key AC06: ISO_Fast_Cursor_Left, ISO_Fast_Cursor_Left
-  key AC07: ISO_Fast_Cursor_Down, ISO_Fast_Cursor_Down
-  key AC08: ISO_Fast_Cursor_Up, ISO_Fast_Cursor_Up
-  key AC09: ISO_Fast_Cursor_Right, ISO_Fast_Cursor_Right
-
-  key AB06: ISO_Partial_Line_Down, ISO_Partial_Line_Down
-  key AD06: ISO_Partial_Line_Up, ISO_Partial_Line_Up
-  key AB08: ISO_Partial_Space_Left, ISO_Partial_Space_Left
-  key AB09: ISO_Partial_Space_Right, ISO_Partial_Space_Right
-
-  key AD07: ISO_Set_Margin_Left, ISO_Set_Margin_Left
-  key AD09: ISO_Set_Margin_Right, ISO_Set_Margin_Right
-
-  key AB07: ISO_Release_Margin_Left, ISO_Release_Margin_Left
-  key AE07: ISO_Release_Margin_Right, ISO_Release_Margin_Right
-  key AD08: ISO_Release_Both_Margins, ISO_Release_Both_Margins
-

commit 43a9f4aefea12aaed67bb37bc3d08cfaccf69f66

linux_console: fix Page_Up/Down not being converted
Fixes: f99fd827 ("hjkl: remove redirs workaround")

conv/linux_console.py
@@ -31,8 +31,8 @@ Translations = {
 
         "Backspace": "BackSpace",
         "Delete": "Remove",
-        "Next": "PageDown",
-        "Prior": "PageUp",
+        "Page_Down": "PageDown",
+        "Page_Up": "PageUp",
 
         "Multi_key": "Compose",
         "Menu": "Last_Console",